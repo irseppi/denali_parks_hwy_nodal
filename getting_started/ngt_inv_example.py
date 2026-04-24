fada27a73e63d8441456fb5aefa35ed80e30cc31
irseppi <irseppi@github.com>
2026-04-24 11:54:22 -0800

    Update file paths

--- getting_started/ngt_inv_example.py
+++ getting_started/ngt_inv_example.py
@@ -6,7 +6,7 @@ from scipy.signal import spectrogram
 
 
 # --- Fix sys.path ---
-repo_root = Path(__file__).resolve().parents[2]
+repo_root = Path(__file__).resolve().parents[1]
 if str(repo_root) not in sys.path:
     sys.path.insert(0, str(repo_root))
 
